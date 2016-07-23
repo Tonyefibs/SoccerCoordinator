1bcd2458838c03646afb30425be8b1c46a4c5527
Tonye Fibs <tonyefibs@tonyes-macbook-pro.local>
2016-07-23 13:58:57 -0400

    commit 1

--- SoccerCoordinator2.playground/Contents.swift
+++ SoccerCoordinator2.playground/Contents.swift
@@ -1,5 +1,75 @@
-//: Playground - noun: a place where people can play
+//Dictionary for each player
+let jSmith = ["Name" : "Joe Smith", "Height" : "42", "Soccer Experience" : "YES", "Guardian Name" : "Jim and Jan Smith"]
 
-import UIKit
+let jTanner = ["Name" : "Jill Tanner", "Height" : "36", "Soccer Experience" : "YES", "Guardian Name" : "Ciara tanner"]
 
-var str = "Hello, playground"
+let bBon = ["Name" : "Bill Bon", "Height" : "43", "Soccer Experience" : "YES", "Guardian Name" : "Sara and Jenny Bon"]
+
+let eGordon = ["Name" : "Eva Gordon", "Height" : "45", "Soccer Experience" : "YNO", "Guardian Name" : "Wendy and Mike Gordon"]
+
+let mGill = ["Name" : "Matt Gill", "Height" : "40", "Soccer Experience" : "NO", "Guardian Name" : "Charles and Sylvia Gill"]
+
+let kStein = ["Name" : "Kimmy Stein", "Height" : "41", "Soccer Experience" : "NO", "Guardian Name" : "Bill and Hillary Stein"]
+
+let sAdams = ["Name" : "Sammy Adams", "Height" : "45", "Soccer Experience" : "NO", "Guardian Name" : "Jeff Adams"]
+
+let kSaygan = ["Name" : "Karl Saygan", "Height" : "42", "Soccer Experience" : "YES", "Guardian Name" : "Heather Bledsoe"]
+
+let sGreenberg = ["Name" : "Suzane Greenberg", "Height" : "44", "Soccer Experience" : "YES", "Guardian Name" : "Henrietta Dumas"]
+
+let sDali = ["Name" : "Sal Dali", "Height" : "41", "Soccer Experience" : "NO", "Guardian Name" : "Gala Dali"]
+
+let jKavalier = ["Name" : "Joe Kavalier", "Height" : "39", "Soccer Experience" : "NO", "Guardian Name" : "Sam and Elaine Kavalier"]
+
+let bFinkelstein = ["Name" : "Ben Finkelstein", "Height" : "44", "Soccer Experience" : "NO", "Guardian Name" : "Aaron and Jill Finkelstein"]
+
+let dSoto = ["Name" : "Diego Soto", "Height" : "41", "Soccer Experience" : "YES", "Guardian Name" : "Robin and Sarika Sotot"]
+
+let cAlaska = ["Name" : "Chloe Alaska", "Height" : "47", "Soccer Experience" : "NO", "Guardian Name" : "David and Jamie Alaska"]
+
+let aWillis = ["Name" : "Arnold Willis", "Height" : "43", "Soccer Experience" : "NO", "Guardian Name" : "Claire Willis"]
+
+let pHelm = ["Name" : "Phillip Helm", "Height" : "44", "Soccer Experience" : "YES", "Guardian Name" : "Thomas Helm and Eva Jones"]
+
+let lClay = ["Name" : "Les Clay", "Height" : "42", "Soccer Experience" : "YES", "Guardian Name" : "Wynonna Brown"]
+
+let hKrustofski = ["Name" : "Herschel Krustofski", "Height" : "45", "Soccer Experience" : "YES", "Guardian Name" : "Hyman and Rachel Krustofski"]
+
+//Empty array for allPlayer in the league
+var allPlayers : [[String : String]] = []
+
+allPlayers.append(jSmith)
+allPlayers.append(jTanner)
+allPlayers.append(bBon)
+allPlayers.append(eGordon)
+allPlayers.append(mGill)
+allPlayers.append(kStein)
+allPlayers.append(sAdams)
+allPlayers.append(kSaygan)
+allPlayers.append(sGreenberg)
+allPlayers.append(sDali)
+allPlayers.append(jKavalier)
+allPlayers.append(bFinkelstein)
+allPlayers.append(dSoto)
+allPlayers.append(cAlaska)
+allPlayers.append(aWillis)
+allPlayers.append(pHelm)
+allPlayers.append(lClay)
+allPlayers.append(hKrustofski)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
